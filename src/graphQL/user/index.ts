d47a74a9557accdd0af295343b11ab10620652e0
Himanshu Yadav <himanshu.yadav@studiographene.com>
2023-08-07 10:23:11 +0530

    fix: dev testing fixes

--- src/graphQL/user/index.ts
+++ src/graphQL/user/index.ts
@@ -1,6 +1,7 @@
 import {useLazyQuery} from '@apollo/client';
 import {FILTER_CUSTOMER_LIST} from './queries';
-import LoggerUtil from '../../utils/LoggerUtil';
+import {Errors} from '../../constants/Strings';
+import {Alert} from 'react-native';
 
 export const useZellerCustomersList = (roleFilter?: string) => {
   const [fetchList, {loading, error, data, previousData, refetch}] =
@@ -12,13 +13,13 @@ export const useZellerCustomersList = (roleFilter?: string) => {
     return {fetchList, loading, data: previousData ?? [], refetchList: refetch};
   }
   if (error) {
-    LoggerUtil.log('Customers list error: ', error);
+    Alert.alert(Errors.alert, error.message);
     return {fetchList, loading, data: previousData ?? [], refetchList: refetch};
   }
   return {
     fetchList,
     loading,
-    data: data?.listZellerCustomers?.items,
+    data: data?.listZellerCustomers?.items ?? [],
     refetchList: refetch,
   };
 };
